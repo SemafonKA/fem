0d55d8f00e424045d23c8e45e6fb2e54eff67f0b
Semyon <semafonplay@gmail.com>
2024-11-09 13:26:52 +0700

    Add some text to `domain.h` file

--- lab1/fem/includes/domain.h
+++ lab1/fem/includes/domain.h
@@ -2,6 +2,7 @@
 
 /// NOTE: This file only includes ALL various Domain structures at once.
 /// Please include this file and then use namespaces (like `using namespace fem::two_dim`) 
-/// to select what Domain realization you need
+/// to select what Domain realization you need.
+/// In general, you can include only one of headers below, but for what?
 
 #include "../two_dim/two_dim_domain.h";
